7ed1017fa46218f0c10c56f0d1874b9e20adf869
unknown <422michaelcerreto@gmail.comgit config --global user.name>
2024-01-12 17:13:28 -0500

    About Me Phone First Draft

--- frontend/Widgets/Title.jsx
+++ frontend/Widgets/Title.jsx
@@ -11,7 +11,7 @@ const Title = ({
       <span className={`h-1 w-12 rounded-sm ${color} relative z-10`}></span>
       <h3
         className={`text-3xl tracking-widest font-light tracking-wide relative z-10 ${
-          dark ? "text-neutral-200" : ""
+          dark ? "text-neutral-200" : "text-neutral-700"
         }`}
       >
         {text}
@@ -20,9 +20,9 @@ const Title = ({
         className={`absolute whitespace-nowrap hidden md:block text-8xl transform ${translateY} ${translateX}`}
       >
         <h1
-          className={`relative z-0 font-normal tracking-widest ${
-            dark ? "text-neutral-200" : ""
-          } text-opacity-5 `}
+          className={`relative z-0 font-normal tracking-widest text-opacity-5 ${
+            dark ? "text-neutral-200" : "text-neutral-700"
+          }  `}
         >
           {backgroundText}
         </h1>
--- frontend/components/About.jsx
+++ frontend/components/About.jsx
@@ -1,18 +1,105 @@
+"use client";
 import Title from "@/Widgets/Title";
 import { useSelector } from "react-redux";
+import "../styles/About.css";
+import Image from "next/image";
+import { IoHome } from "react-icons/io5";
+import { phoneIcons } from "@/data";
+import { useState, useEffect } from "react";
+
 const About = () => {
   const dark = useSelector((state) => state.auth.dark);
+  const [icons, setIcons] = useState(null);
+
+  // render in icons upon load
+  useEffect(() => {
+    setIcons(phoneIcons);
+  }, []);
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <section
+      id="about"
+      className="min-h-screen flex flex-col items-center justify-center gap-6"
+    >
       <Title
         text={"About"}
-        backgroundText={"About Me"}
+        backgroundText={"<About Me />"}
         dark={dark}
         color={"bg-blue-400"}
-        translateX="-translate-x-32"
+        translateX="-translate-x-64"
         translateY={"-translate-y-2"}
       />
-    </div>
+      {/* PHONE IN THE CENTER OF THE SCREEN */}
+      <div
+        className={`phone flex flex-col p-2 m-0 rounded-xl px-8 py-2  justify-center items-center ${
+          dark ? "dark-shadow" : "light-shadow"
+        }`}
+      >
+        {/* PHONE HEADER */}
+        <div className="flex flex-col items-center justify-center">
+          <h1>Michel Cerreto</h1>
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-row justify-between ">
+              <p>Age:</p>
+              <p>Education:</p>
+              <p>From:</p>
+            </div>
+            <div className="flex flex-row justify-between ">
+              <p>19</p>
+              <p>Rutgers</p>
+              <p>New Jersey</p>
+            </div>
+          </div>
+        </div>
+
+        <Image
+          src={`${
+            dark ? "/images/darkHeadshot.png" : "/images/lightHeadShot.png"
+          } `}
+          alt="picture of me"
+          height={140}
+          width={180}
+        />
+
+        <div>
+          <h3>Software Engineer</h3>
+          <p className="text-sm">retto12345678@gmail.com</p>
+        </div>
+        {/* PHONE ICONS */}
+
+        {icons != null ? (
+          <div className="grid grid-cols-3 gap-4">
+            {icons.map((icon, index) => (
+              <div key={index}>
+                <Image
+                  src={icon.src}
+                  alt={`${icon.icon} logo`}
+                  height={55}
+                  width={55}
+                />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )}
+
+        {/* PHONE FOOTER */}
+        <p>@2024, All Rights Reserved</p>
+        <hr className="m-0 p-0"></hr>
+        <div className="flex flex-row items-center justify-between">
+          <div>
+            <p>View My Skills</p>
+          </div>
+          <div>
+            <IoHome />
+          </div>
+          <div>
+            <p>View My Projects</p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
