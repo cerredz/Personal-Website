e618381f325ef9addcb144c218839e65fe32921e
cerredz <422michaelcerreto@gmail.com>
2024-02-23 20:05:41 -0500

    Projects Read More Background Design + Icons

--- frontend/app/projects/[projectTitle]/page.jsx
+++ frontend/app/projects/[projectTitle]/page.jsx
@@ -11,6 +11,11 @@ import Image from "next/image";
 import { client, urlFor } from "@/utils/Sanity";
 import { motion } from "framer-motion";
 import { viewMoreProjects } from "@/utils/Project";
+import { IoLogoReact } from "react-icons/io5";
+import { IoCodeSlash } from "react-icons/io5";
+
+import "./styles.css";
+
 export default function Page() {
   const dark = useSelector((state) => state.auth.dark);
   const pathname = usePathname();
@@ -39,11 +44,23 @@ export default function Page() {
     <div>
       {projectData !== null ? (
         <>
-          <main
+          <div
             className={`overflow-hidden font-primary p-0 m-0 min-h-screen ${
               dark ? "bg-primary-dark" : "bg-primary-light"
             } flex flex-col xl:flex-row gap-20 items-center justify-center px-20 xl:px-36 mx-auto pt-40 pb-20 `}
           >
+            {/* BACKGOUND ICONS */}
+            <BackgroundIcons />
+            {/* BACKGROUND IMAGE */}
+            <Image
+              quality={100}
+              priority={true}
+              src={"/images/projects/readMoreBackground.png"}
+              alt=""
+              width={1920}
+              height={1080}
+              className="absolute z-1 top-0 left-0 w-full h-[full] background-img hidden xl:block"
+            />
             {/* IMAGE CONTAINER */}
             <div className="flex items-center justify-center xl:basis-3/5 ">
               <Image
@@ -53,12 +70,12 @@ export default function Page() {
                 alt={projectData.title}
                 height={1000}
                 width={1000}
-                className="  rounded-2xl"
+                className="z-10  rounded-2xl"
                 sizes=""
               />
             </div>
             {/* TEXT CONTAINER */}
-            <div className="flex flex-col items-start justify-start xl:basis-2/5 gap-6">
+            <div className="flex flex-col items-start justify-start xl:basis-2/5 gap-6 z-10">
               <h1
                 style={{
                   backgroundImage: `${projectData.textColor}`,
@@ -66,7 +83,7 @@ export default function Page() {
                   WebkitBackgroundClip: "text",
                   color: "transparent",
                 }}
-                className={`font-bold text-5xl tracking-widest`}
+                className={`font-bold text-5xl tracking-widest text-center`}
               >
                 {projectData.title}
               </h1>
@@ -168,24 +185,24 @@ export default function Page() {
                 </p>
               )}
             </div>
-          </main>
-          <div
-            className={` flex items-center justify-center pb-10 ${
-              dark ? "bg-primary-dark" : "bg-primary-light"
-            }`}
-          >
-            <motion.button
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              style={{
-                background: `${projectData.textColor}`,
-                boxShadow: "inset 0 0 15px rgba(255,255,255,.2)",
-              }}
-              className={`px-6 py-2 rounded-lg italic font-bold tracking-wider text-neutral-300 `}
-              onClick={() => viewMoreProjects()}
+            <div
+              className={`absolute bottom-[20px] flex items-center justify-center pb-10 ${
+                dark ? "bg-primary-dark" : "bg-primary-light"
+              }`}
             >
-              View More Projects
-            </motion.button>
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                style={{
+                  background: `${projectData.textColor}`,
+                  boxShadow: "inset 0 0 15px rgba(255,255,255,.2)",
+                }}
+                className={`px-6 py-2 rounded-lg italic font-bold tracking-wider text-neutral-300 `}
+                onClick={() => viewMoreProjects()}
+              >
+                View More Projects
+              </motion.button>
+            </div>
           </div>
         </>
       ) : (
@@ -194,3 +211,25 @@ export default function Page() {
     </div>
   );
 }
+
+const BackgroundIcons = () => {
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 0.25, y: 0, transition: { delay: 0.6 } }}
+        className="absolute bottom-[150px] left-[25px] z-10 hidden xl:block"
+      >
+        <IoLogoReact className="text-5xl rotate-45 text-neutral-500 " />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 0.25, x: 0, transition: { delay: 0.8 } }}
+        className="absolute bottom-[40px] left-[300px] z-10"
+      >
+        <IoCodeSlash className="text-5xl -rotate-12 text-neutral-500 hidden xl:block" />
+      </motion.div>
+    </>
+  );
+};
